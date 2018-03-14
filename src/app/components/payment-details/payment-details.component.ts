c6a88521b565bc4af78e1ef56ff20e95d5ef8044
krosenk729 <krosenk729@gmail.com>
2018-03-14 09:28:58 -0400

    firebase version issues fixed

--- src/app/components/payment-details/payment-details.component.ts
+++ src/app/components/payment-details/payment-details.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,, Input } from '@angular/core';
 
 
 @Component({
-  selector: 'app-payment-details',
-  templateUrl: './payment-details.component.html'
+	selector: 'app-payment-details',
+	templateUrl: './payment-details.component.html'
 })
 export class PaymentDetailsComponent implements OnInit {
+	@Input() payment;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
