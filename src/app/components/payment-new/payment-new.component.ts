8aa3d3d86554188402f3fc7b9ace6aca1bf20b8f
krosenk729 <krosenk729@gmail.com>
2018-04-29 17:49:25 -0400

    added reactive forms

--- src/app/components/payment-new/payment-new.component.ts
+++ src/app/components/payment-new/payment-new.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-payment-new',
-  templateUrl: './payment-new.component.html'
+	selector: 'app-payment-new',
+	templateUrl: './payment-new.component.html'
 })
 export class PaymentNewComponent implements OnInit {
+	newPaymentForm: FormGroup;
+	constructor(private formBuilder: FormBuilder) { }
 
-  constructor() { }
+	createForm() {
+		this.newPaymentForm = this.formBuilder.group({
+			name: ['', Validators.required],
+			url: '',
+			cost: ['', Validators.required],
+			freq: '',
+			startDate: ['', Validators.required]
+		});
+	}
 
-  ngOnInit() {
-  }
+	onFormSubmit(){
+		console.log( this.newPaymentForm );
+	}
+
+	ngOnInit() {
+		this.createForm();
+	}
 
 }
