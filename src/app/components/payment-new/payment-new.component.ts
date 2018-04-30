9ed56a59b3b1ee4686f27c9e75ba430acf2bfff5
krosenk729 <krosenk729@gmail.com>
2018-04-30 06:08:47 -0400

    adding default form values with moment

--- src/app/components/payment-new/payment-new.component.ts
+++ src/app/components/payment-new/payment-new.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { MyFireService } from '../../providers/myfire.service';
 import * as firebase from 'firebase';
+import * as Moment from 'moment';
 
 @Component({
 	selector: 'app-payment-new',
@@ -15,17 +16,17 @@ export class PaymentNewComponent implements OnInit {
 		this.newPaymentForm = this.formBuilder.group({
 			name: ['', Validators.required],
 			url: '',
-			cost: ['', Validators.required],
-			freq: '',
-			startDate: ['', Validators.required]
+			cost: ['11', Validators.required],
+			freq: 'Monthly',
+			startDate: [moment().format('YYYY-MM-DD'), Validators.required]
 		});
 	}
 
 	onFormSubmit(){
-		if(!this.newPaymentForm.errors){
-			this.myFire.handleNewPayment( this.newPaymentForm.value );
-			this.newPaymentForm.reset();
-		}
+		// if(!this.newPaymentForm.errors){
+		// 	this.myFire.handleNewPayment( this.newPaymentForm.value );
+		// 	this.newPaymentForm.reset();
+		// }
 		console.log( this.newPaymentForm );
 	}
 
